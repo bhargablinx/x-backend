ce7457ba40d182713081a5cacdd10631aca9dd89
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-13 14:11:39 +0530

    add follow schema

---
+++ src/models/follow.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const followSchema = new Schema(
+    {
+        follower: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        following: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+followSchema.index({ follower: 1, following: 1 }, { unique: true });
+
+export default Follow = mongoose.model("Follow", followSchema);
