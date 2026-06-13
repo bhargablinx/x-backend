e8a504ae297083d3b3f0ad5f710ea6ef91832405
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-13 21:09:11 +0530

    password is hashed before saving

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
     {
@@ -47,4 +48,11 @@ const userSchema = new Schema(
     { timestamps: true }
 );
 
-export default User = mongoose.model("User", userSchema);
+userSchema.pre("save", async function () {
+    if (this.isModified("password"))
+        this.password = await bcrypt.hash(this.password, 10);
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
