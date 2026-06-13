8b1ee76cbbd0179e608ea2b80aaba91fb4087ee8
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-13 13:46:05 +0530

    add post schema

---
+++ src/models/post.model.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema(
+    {
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+            trim: true,
+            maxLength: 280,
+        },
+        likesCount: {
+            type: Number,
+            default: 0,
+        },
+        commentCount: {
+            type: Number,
+            default: 0,
+        },
+    },
+    { timestamps: true }
+);
+
+export default Post = mongoose.model("Post", postSchema);
