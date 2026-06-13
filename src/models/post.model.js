04b890c9e1bfd6454b16458074eea3ba0e1f97d3
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-13 13:57:55 +0530

    fix type maxLength -> maxlength

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -11,7 +11,7 @@ const postSchema = new Schema(
             type: String,
             required: true,
             trim: true,
-            maxLength: 280,
+            maxlength: 280,
         },
         likesCount: {
             type: Number,
