f1a89ce6ef9fd2d9554f9027134822e393421704
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-13 20:44:31 +0530

    add register user func

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,49 @@
-const registerUsr = (req, res) => {
-    res.send("User registered!!");
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import User from "../models/user.model.js";
+
+const registerUsr = async (req, res) => {
+    try {
+        const { username, name, email, password, bio, avatar } = req.body;
+
+        if (!username || !name || !email || !password)
+            res.status(400).json({ message: "Enter all required fields" });
+
+        // check if user already exit
+        const dbRes = await User.find({ email });
+        if (dbRes.length !== 0)
+            return res.status(400).json({
+                message: "User already exits!",
+            });
+
+        // check of avatar -> if available handle later (not now)
+
+        // create a user and push to db
+        const user = await User.create({
+            username,
+            name,
+            email,
+            password,
+            bio,
+            avatar,
+        });
+
+        // send to response to client
+        res.status(200).send({
+            message: "Account Created",
+            user,
+        });
+    } catch (error) {
+        res.status(500).send({
+            message: "Error registering user!!",
+        });
+    }
 };
 
 const loginUsr = (req, res) => {
-    res.send("User Logged in!!");
+    res.status(200).json({
+        message: "OK",
+    });
 };
 
 const logoutUsr = (req, res) => {
