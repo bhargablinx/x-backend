94450bba2b7be4661c27a460fcf6731f3fe29101
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-14 15:39:09 +0530

    add create post func

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,5 +1,26 @@
-const createPost = (req, res) => {
-    res.send("Post add!!");
+import Post from "../models/post.model.js";
+
+const createPost = async (req, res) => {
+    try {
+        const { content } = req.body;
+
+        if (!content)
+            return res.status(401).json({
+                message: "Content is required!!",
+            });
+
+        const userId = req.user._id;
+
+        const post = await Post.create({
+            author: userId,
+            content,
+        });
+
+        res.status(200).json(post);
+    } catch (error) {
+        throw error;
+        res.status(500).json({ message: "Failed creating post" });
+    }
 };
 
 const getAllPosts = (req, res) => {
