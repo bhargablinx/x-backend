6b0aac9629e097962680218b69443bb9e19c38d8
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-14 21:22:14 +0530

    add follow user func

--- src/controllers/follow.controller.js
+++ src/controllers/follow.controller.js
@@ -1,11 +1,41 @@
+import Follow from "../models/follow.model.js";
+import User from "../models/user.model.js";
+
 const followUser = async (req, res) => {
     try {
+        const loggedInUserId = req.user._id;
+        const followedUserId = req.params.userId;
+
+        if (loggedInUserId.toString() === followedUserId)
+            res.status(404).json({
+                message: "Can't self-follow",
+            });
+
+        const user = await User.findById(followedUserId);
+        if (!user)
+            res.status(404).json({
+                message: "User don't exits that you want to follow",
+            });
+
+        await Follow.create({
+            follower: loggedInUserId,
+            following: followedUserId,
+        });
+
         res.status(200).json({
-            message: "Followed!!",
+            message: "User followed successfully",
         });
     } catch (error) {
+        if (error.code === 11000) {
+            return res.status(400).json({
+                message: "Already following this user",
+            });
+        }
+
         console.log(error);
-        res.status(500).json({ message: "Something went wrong" });
+        return res.status(500).json({
+            message: "Something went wrong",
+        });
     }
 };
 
