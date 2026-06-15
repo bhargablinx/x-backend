22e2b25f87d056b4c2a12e9b9dcfa5b11afc3a40
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-15 23:27:13 +0530

    add following feed

--- src/controllers/feed.controller.js
+++ src/controllers/feed.controller.js
@@ -1,3 +1,6 @@
+import Follow from "../models/follow.model.js";
+import Post from "../models/post.model.js";
+
 const homeFeed = async (req, res) => {
     try {
         res.status(200).json({
@@ -11,12 +14,22 @@ const homeFeed = async (req, res) => {
 
 const followingFeed = async (req, res) => {
     try {
-        res.status(200).json({
-            message: "Following feed",
+        const userId = req.user._id;
+        // in follow collection: find where return users where follower = loggedIn user
+        const followingUsers = await Follow.find({ follower: userId }).select(
+            "-_id -createdAt -updatedAt -follower -__v"
+        );
+
+        const followingIds = followingUsers.map((user) => user.following);
+
+        const followingPosts = await Post.find({
+            author: { $in: followingIds },
         });
+
+        res.status(200).json(followingPosts);
     } catch (error) {
         console.log(error);
-        res.status(500).json({ message: "Something went wrong" });
+        res.status(500).json({ message: "Failed to fetch followings post" });
     }
 };
 
