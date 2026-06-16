44b69f78bda658e59cd2956c423a212b4443f7a7
bhargablinx <bhargabbhuyan001@gmail.com>
2026-06-16 16:54:41 +0530

    upd multer middleware to save file in storage

--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -1,3 +1,14 @@
 import multer from "multer";
 
-export const upload = multer({ storage: multer.memoryStorage() });
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    },
+});
+
+export const upload = multer({
+    storage,
+});
